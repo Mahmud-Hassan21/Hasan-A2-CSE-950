b50dcb3570ab55f745858bde16293c4cb33161ab
Hasan <mahmudhassan156813@gmail.com>
2021-05-10 02:33:05 +0600

    03/05/2021 Simple Program & Basic Calculator

---
+++ Lab 01/Job1.java
@@ -0,0 +1,22 @@
+package test.project;
+
+//Class
+public class Job1 {
+
+    public static void main(String[] args) {
+        
+        System.out.print("\nOdd Numbers: ");
+        
+        //Loop
+        for (int i = 1; i <= 10; i++) {
+            
+            //Condition
+            if (i % 2 == 1) {
+                
+                //Display
+                System.out.print(" "+i+" ");
+            }
+            
+        }
+    }
+}
